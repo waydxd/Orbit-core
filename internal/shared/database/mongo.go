5bc133a3c9657df035e47814de5508ef0dfb691a
Wayd <wychancf@connect.ust.hk>
2025-12-15 00:15:02 +0800

    Add MongoDB initialization and connection management

---
+++ internal/shared/database/mongo.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoClient *mongo.Client
+
+func InitMongoDB(uri string) {
+	var err error
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	MongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		cancel()
+		log.Printf("Failed to connect to MongoDB: %v", err)
+	}
+
+	// Check the connection
+	err = MongoClient.Ping(ctx, nil)
+	if err != nil {
+		cancel()
+		log.Printf("Failed to ping MongoDB: %v", err)
+	}
+
+	log.Println("Connected to MongoDB!")
+}
+
+func DisconnectMongoDB() {
+	if MongoClient != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := MongoClient.Disconnect(ctx); err != nil {
+			cancel()
+			log.Printf("Failed to disconnect from MongoDB: %v", err)
+		}
+	}
+}
