dedbb6fa3ca9136c458cdc630348dba8f2e0490c
Wayd <wychancf@connect.ust.hk>
2025-12-18 21:02:45 +0800

    Refactor InitMongoDB to return error on connection failure

--- internal/shared/database/mongo.go
+++ internal/shared/database/mongo.go
@@ -11,25 +11,26 @@ import (
 
 var MongoClient *mongo.Client
 
-func InitMongoDB(uri string) {
+func InitMongoDB(uri string) error {
 	var err error
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	MongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
-		cancel()
 		log.Printf("Failed to connect to MongoDB: %v", err)
+		return err
 	}
 
 	// Check the connection
 	err = MongoClient.Ping(ctx, nil)
 	if err != nil {
-		cancel()
 		log.Printf("Failed to ping MongoDB: %v", err)
+		return err
 	}
 
 	log.Println("Connected to MongoDB!")
+	return nil
 }
 
 func DisconnectMongoDB() {
